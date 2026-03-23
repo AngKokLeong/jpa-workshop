a4fcb8a8906b4f3cce41a7df2ca3d118bf685836
Ang Kok Leong <ang.kok.leong.development@gmail.com>
2026-03-23 14:26:10 +0000

    added CourseRepository

---
+++ src/main/java/sg/nus/edu/empdemo/repository/CourseRepository.java
@@ -0,0 +1,35 @@
+package sg.nus.edu.empdemo.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import sg.nus.edu.empdemo.model.Course;
+import java.time.LocalDate;
+
+
+public interface CourseRepository extends JpaRepository<Course, Long> {
+    
+    // Find courses where the name contains a specific string, ignoring case
+    public List<Course> findByNameContainingAllIgnoringCase(String name);
+    
+    //https://docs.spring.io/spring-data/jpa/reference/jpa/query-methods.html
+    //Find course starting after a specific date
+    public List<Course> findByStartsGreaterThan(LocalDate starts);
+
+    //Find courses by maximum duration
+    public List<Course> findBydurationInMonthsOrderByDesc();
+
+    //Find courses by employeeId
+    public List<Course> findByEmployeesEquals(Long id);
+
+    //Fetch a course and its associated Employee in a single query by the Course ID 
+    public Course findByIdWithEmployee(Long courseId);
+
+    // Find courses by employeeId that start after a specific date
+    //@Query("select u.employees from courses where u.starts = ?1")
+    //public List<Course> findBy();
+
+
+}
